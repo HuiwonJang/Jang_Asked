18be65a89c77477e8ef1e63ce11920d73f1d31d3
HuiwonJang <69646951+HuiwonJang@users.noreply.github.com>
2021-07-06 19:11:28 +0900

    Add files via upload

---
+++ js/show_data.js
@@ -0,0 +1,26 @@
+$.ajax({
+  url: 'asked_data.csv',
+  dataType: 'text',
+  contentType: "application/x-www-form-urlencoded; charset=euc-kr",
+}).done(successFunction);
+
+function successFunction(data){
+  var allRows = data.split(/\r?\n|\r/);
+  var table = '<table>';
+
+  for (var singleRow = 0; singleRow < allRows.length; singleRow++){
+    table += '<tr>';
+
+    var rowCells = allRows[singleRow].split(',');
+    for (var rowCell = 0; rowCell < rowCells.length; rowCell++){
+      table += '<td>';
+      table += rowCells[rowCell];
+      table += '</td>';
+    }
+    table += '<\tr>';
+  }
+  table += '</tbody>';
+  table += '</table>';
+
+  $('#div_qnas').append(table);
+}
